f51d830caf4f98dcdec87f9bdab7c559286af547
toluboy <tolualonge123@gmail.com>
2022-07-25 10:19:23 +0100

    forgot password page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mr_receipe/screens/login-screen.dart';
+import 'package:mr_receipe/Screens/forgot-password.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,20 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Recipe Guide',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: LoginScreen(),
+      routes: {
+        'ForgotPassword': (context) => ForgotPassword(),
+      },
     );
   }
 }
-
